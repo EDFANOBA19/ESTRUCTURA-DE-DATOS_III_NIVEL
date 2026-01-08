4ed40f809242f3a77c33c697e15989bad8f10c5a
EDFANOBA19 <efnoriegab@gmail.com>
2026-01-07 22:27:47 -0500

    Implementación de ejercicios con listas en C# usando POO
    Se resolvieron 5 ejercicios utilizando List y Dictionary en C#,
    aplicando Programación Orientada a Objetos.

--- SEMANA_5/ejercicio4/obj/Debug/net10.0/ejercicio4.AssemblyInfo.cs
+++ SEMANA_5/ejercicio4/obj/Debug/net10.0/ejercicio4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ejercicio4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3134eb07e8c2dc7780e3d91c6cf0489b806f16d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eff73081d4898c913408d23c5af4ca69304bf35")]
 [assembly: System.Reflection.AssemblyProductAttribute("ejercicio4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ejercicio4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
